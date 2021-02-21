6d1e83a638ef0f3e9723215129ab73c48a055e7c
sergeikharchikov <sergei.kharchikov@nordigy.ru>
2021-02-22 00:50:51 +0300

    Add exercises 3.3 - 3.6

--- src/main/scala/com/callmestech/exercises/chapter3/package.scala
+++ src/main/scala/com/callmestech/exercises/chapter3/package.scala
@@ -8,6 +8,18 @@ package object chapter3 {
 
   sealed trait List[+A] extends Product with Serializable {
 
+    def ::[B >: A](b: B): Cons[B] = Cons(b, this)
+
+    def reverse: List[A] = {
+      @tailrec
+      def loop(as: List[A], acc: List[A]): List[A] = as match {
+        case Nil => acc
+        case Cons(h, t) => loop(t, h :: acc)
+      }
+
+      loop(this, Nil)
+    }
+
     def foldL[B](z: B)(f: (B, A) => B): B = {
       @tailrec
       def loop(z: B)(f: (B, A) => B, rem: List[A]): B = rem match {
@@ -19,13 +31,14 @@ package object chapter3 {
 
     def reduce[B >: A](f: (B, B) => B): B = this match {
       case Nil => throw new UnsupportedOperationException("Empty.reduce")
-      case Cons(h, t) => {
+      case Cons(h, t) =>
+        @tailrec
         def loop(f: (B, B) => B, acc: B, rem: List[B]): B = rem match {
           case Nil => acc
           case Cons(h, t) => loop(f, f(acc, h), t)
         }
+
         loop(f, h, t)
-      }
     }
 
 
@@ -39,6 +52,54 @@ package object chapter3 {
       case Cons(_, t) => t
     }
 
+    /** Exercise 3.3
+     *
+     * Using the same idea, implement the function setHead
+     * for replacing the first element of a List with a different value.
+    **/
+    def setHead[B >: A](b: B): List[B] = this match {
+      case Nil => Cons(b, Nil)
+      case Cons(_, t) => Cons(b, t)
+    }
+
+    /** Exercise 3.4
+     *
+     * Generalize tail to the function drop, which removes the first n elements from a list.
+     * Note that this function takes time proportional only to the number of elements
+     * being dropped—we don’t need to make a copy of the entire List.
+     **/
+    def drop(n: Int): List[A] =
+      if (n <= 0) this
+      else tail.drop(n - 1)
+
+    /** Exercise 3.5
+     *
+     * Implement dropWhile, which removes elements from the List prefix as long as they match a predicate.
+     **/
+    def dropWhile(p: A => Boolean): List[A] = this match {
+      case Nil => this
+      case Cons(h, t) => if (p(h)) t.dropWhile(p) else this
+    }
+
+    /** Exercise 3.6
+     *
+     * Not everything works out so nicely. Implement a function, init,
+     * that returns a List consisting of all but the last element of a List. So, given List(1,2,3,4),
+     * init will return List(1,2,3).
+     * Why can’t this function be implemented in constant time like tail?
+     *
+     * My implementation has O(N^2^) complexity. It's horrible. How could I do better?
+    **/
+    def init: List[A] = {
+      @tailrec
+      def loop(as: List[A], acc: List[A]): List[A] = as match {
+        case Nil          => acc
+        case Cons(_, Nil) => acc
+        case Cons(h, t)   => loop(t, Cons(h, acc))
+      }
+
+      loop(this, Nil).reverse
+    }
   }
 
   final case object Nil extends List[Nothing]

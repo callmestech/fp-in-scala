eb8dc74a4446e82697e7f2ee71c87e201f4cb4b2
sergeikharchikov <sergei.kharchikov@nordigy.ru>
2021-03-13 19:59:31 +0300

    Add exercises 4.3 - 4.4

--- src/main/scala/com/callmestech/exercises/chapter4/MyOption.scala
+++ src/main/scala/com/callmestech/exercises/chapter4/MyOption.scala
@@ -1,5 +1,7 @@
 package com.callmestech.exercises.chapter4
 
+import scala.annotation.tailrec
+
 sealed trait MyOption[+A] {
   import MyOption._
 
@@ -42,6 +44,22 @@ sealed trait MyOption[+A] {
     case Some(_) => false
     case None    => true
   }
+
+  /*
+  def lift[A1 >: A, B >: A](f: A1 => B): MyOption[A1] => MyOption[B] =
+    _.map(f)
+
+  val optAbs = lift(math.abs) */
+
+  /** Exercise 4.3
+   *
+   * Write a generic function map2 that combines two Option values using a binary function.
+   * If either Option value is None, then the return value is too. Here is its signature:
+   * def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C]
+   * */
+  def map2[B, C](other: MyOption[B])
+                (f: (A, B) => C): MyOption[C] =
+    flatMap(a => other.map(b => f(a, b)))
 }
 
 object MyOption {
@@ -62,4 +80,38 @@ object MyOption {
 
     mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
   }
+
+  /** Exercise 4.3
+   *
+   * */
+
+  def map2[A, B, C](a: MyOption[A], b: MyOption[B])
+                   (f: (A, B) => C): MyOption[C] =
+    a.flatMap(aa => b.map(bb => f(aa, bb)))
+
+  /** Exercise 4.4
+   *
+   * Write a function sequence that combines a list of Options into one Option containing
+   * a list of all the Some values in the original list.
+   * If the original list contains None even once, the result of the function should be None;
+   * otherwise the result should be Some with a list of all the values.
+   * */
+  def sequence[A](xs: List[MyOption[A]]): MyOption[List[A]] = {
+
+    @tailrec
+    def go(xs: List[MyOption[A]], acc: MyOption[List[A]]): MyOption[List[A]] = xs match {
+      case Nil => acc
+      case ::(head, next) => go(next, head.flatMap(a => acc.map(as => a :: as)))
+    }
+
+    if (xs.isEmpty) None else go(xs, Some(Nil))
+  }
+
+  def sequence2[A](xs: List[MyOption[A]]): MyOption[List[A]] = xs match {
+    case Nil => Some(Nil)
+    case ::(head, next) => head.flatMap(h => sequence2(next).map(h :: _))
+  }
+
+  def sequenceViaMap2[A](xs: List[MyOption[A]]): MyOption[List[A]] =
+    xs.foldRight(Some(Nil): MyOption[List[A]])((acc, a) => acc.map2(a)(_ :: _))
 }

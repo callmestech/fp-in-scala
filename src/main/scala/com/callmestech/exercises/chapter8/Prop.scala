feda2b64d6abc8071718c971fc86c201423358a2
sergeikharchikov <sergei.kharchikov@nordigy.ru>
2021-05-15 17:14:14 +0300

    Exercises 8.1 - 8.8

--- src/main/scala/com/callmestech/exercises/chapter8/Prop.scala
+++ src/main/scala/com/callmestech/exercises/chapter8/Prop.scala
@@ -1,13 +1,16 @@
 package com.callmestech.exercises.chapter8
 
 trait Prop {
-  def check: Boolean
+  type FailedCase = String
+  type SuccessCount = Int
+
+  def check: Either[(FailedCase, SuccessCount), SuccessCount]
 
   /** Exercise 8.3
    *
    * Assuming the following representation of Prop, implement && as a method of Prop.
    * */
-  def &&(p: Prop): Prop = new Prop {
-    override def check: Boolean = Prop.this.check && p.check
-  }
+//  def &&(p: Prop): Prop = new Prop {
+//    override def check: Boolean = Prop.this.check && p.check
+//  }
 }

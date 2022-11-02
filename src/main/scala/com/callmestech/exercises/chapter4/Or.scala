db2f1f2d31847bd65d28ccb5e99a9fc132393e46
Sergei Kharchikov <skharchikov@evolution.com>
2022-11-02 17:54:57 +0000

    Update ci script

--- src/main/scala/com/callmestech/exercises/chapter4/Or.scala
+++ src/main/scala/com/callmestech/exercises/chapter4/Or.scala
@@ -1,14 +1,14 @@
 package com.callmestech.exercises.chapter4
 
-/**
- *  Named Or instead of either to avoid collisions with built-in type
- * */
+/**  Named Or instead of either to avoid collisions with built-in type
+  */
 sealed trait Or[+E, +A] {
   import Or._
+
   /** Exercise 4.6
-   *
-   * Implement versions of map, flatMap, orElse, and map2 on Either that operate on the Right value.
-   * */
+    *
+    * Implement versions of map, flatMap, orElse, and map2 on Either that operate on the Right value.
+    */
 
   def map[B](f: A => B): E Or B = this match {
     case l @ Left(_) => l
@@ -19,13 +19,12 @@ sealed trait Or[+E, +A] {
     case Right(value) => f(value)
   }
 
-  def orElse[EE >: E,B >: A](b: => EE Or B): EE Or B = this match {
+  def orElse[EE >: E, B >: A](b: => EE Or B): EE Or B = this match {
     case r @ Right(_) => r
     case _            => b
   }
 
-  def map2[EE >: E, B, C](b: EE Or B)
-                         (f: (A, B) => C): EE Or C = {
+  def map2[EE >: E, B, C](b: EE Or B)(f: (A, B) => C): EE Or C = {
     for {
       a <- this
       b <- b
@@ -41,19 +40,20 @@ object Or {
     try Right(a)
     catch {
       case e: Exception => Left(e)
-  }
+    }
 
   /** Exercise 4.7
-   *
-   * Implement sequence and traverse for Either.
-   * These should return the first error that’s encountered, if there is one.
-   *  {{{def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]]}}}
-   *  {{{def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]]}}}
-   * */
+    *
+    * Implement sequence and traverse for Either.
+    * These should return the first error that’s encountered, if there is one.
+    *  {{{def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]]}}}
+    *  {{{def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]]}}}
+    */
   def sequence[E, A](es: List[Or[E, A]]): E Or List[A] =
     traverse(es)(identity)
 
-  def traverse[E, A, B](as: List[A])
-                       (f: A => Or[E, B]): Or[E, List[B]] =
-    as.foldRight(Right(List.empty[B]): Or[E, List[B]])((h, t) => f(h).map2(t)(_ :: _))
+  def traverse[E, A, B](as: List[A])(f: A => Or[E, B]): Or[E, List[B]] =
+    as.foldRight(Right(List.empty[B]): Or[E, List[B]])((h, t) =>
+      f(h).map2(t)(_ :: _)
+    )
 }
--- src/main/scala/com/callmestech/exercises/chapter8/Prop.scala
+++ src/main/scala/com/callmestech/exercises/chapter8/Prop.scala
@@ -1,38 +1,45 @@
 package com.callmestech.exercises.chapter8
 
 import com.callmestech.exercises.chapter6.{RNG, SimpleRNG}
-import com.callmestech.exercises.chapter8.Prop.{FailedCase, MaxSize, SuccessCount, TestCases}
+import com.callmestech.exercises.chapter8.Prop.{
+  FailedCase,
+  MaxSize,
+  SuccessCount,
+  TestCases
+}
 import com.callmestech.exercises.chapter8.Result.{Falsified, Passed}
 
 case class Prop(run: (MaxSize, TestCases, RNG) => Result) {
   def &&(p: Prop): Prop = Prop { (max, cases, rng) =>
     run(max, cases, rng) match {
       case Passed => p.run(max, cases, rng)
-      case x => x
+      case x      => x
     }
   }
 
   def ||(p: Prop): Prop = Prop { (max, cases, rng) =>
     run(max, cases, rng) match {
       case Falsified(_, _) => p.run(max, cases, rng)
-      case x => x
+      case x               => x
     }
   }
 }
 
 object Prop {
-  type FailedCase = String
-  type TestCases = Int
+  type FailedCase   = String
+  type TestCases    = Int
   type SuccessCount = Int
-  type MaxSize = Int
+  type MaxSize      = Int
 
   def apply(f: (TestCases, RNG) => Result): Prop =
-    Prop { (_,n,rng) => f(n,rng) }
+    Prop { (_, n, rng) => f(n, rng) }
 
-  def run(prop: Prop,
-          maxSize: MaxSize = 100,
-          testCases: TestCases = 100,
-          rng: RNG = SimpleRNG(System.nanoTime())): Unit =
+  def run(
+      prop: Prop,
+      maxSize: MaxSize = 100,
+      testCases: TestCases = 100,
+      rng: RNG = SimpleRNG(System.nanoTime())
+  ): Unit =
     prop.run(maxSize, testCases, rng) match {
       case Falsified(failure, successes) =>
         println(s"! Falsified after $successes passed tests:\n $failure")
@@ -40,29 +47,36 @@ object Prop {
         println(s"+ OK passed $testCases tests.")
     }
 
-  def forAll[A](gen: Int => Gen[A])(f: A => Boolean): Prop = Prop { (max, n, rng) =>
-    val casesPerSize = (n + (max - 1)) / max
-    val props: LazyList[Prop] =
-      LazyList.from(0).take(n.min(max) + 1).map(i => forAll(gen(i))(f))
-    val prop: Prop =
-      props.map(p => Prop { (max, _, rng) =>
-        p.run(max, casesPerSize, rng)
-      }).toList.reduce(_ && _)
-    prop.run(max, n, rng)
+  def forAll[A](gen: Int => Gen[A])(f: A => Boolean): Prop = Prop {
+    (max, n, rng) =>
+      val casesPerSize = (n + (max - 1)) / max
+      val props: LazyList[Prop] =
+        LazyList.from(0).take(n.min(max) + 1).map(i => forAll(gen(i))(f))
+      val prop: Prop =
+        props
+          .map(p =>
+            Prop { (max, _, rng) =>
+              p.run(max, casesPerSize, rng)
+            }
+          )
+          .toList
+          .reduce(_ && _)
+      prop.run(max, n, rng)
   }
 
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = Prop { (n, rng) =>
     randomLazyList(gen)(rng)
       .zip(LazyList.from(0))
       .take(n)
-      .map {
-        case (a, i) => try {
+      .map { case (a, i) =>
+        try {
           if (f(a)) Passed
           else Falsified(a.toString, i)
         } catch {
           case e: Exception => Falsified(buildMsg(a, e), i)
         }
-      }.find(_.isFalsified)
+      }
+      .find(_.isFalsified)
       .getOrElse(Passed)
   }
 
@@ -85,8 +99,8 @@ object Result {
     override def isFalsified: Boolean = false
   }
 
-  final case class Falsified(failure: FailedCase,
-                             successes: SuccessCount) extends Result {
+  final case class Falsified(failure: FailedCase, successes: SuccessCount)
+      extends Result {
     override def isFalsified: Boolean = true
   }
 }
